17f3ffadb1ed9cf00110f313a853f626e99be3b2
tinvdt <thanhtin1808@gmail.com>
2015-03-16 16:28:59 +0700

    print: Test again

--- Test/src/torai/test/Test1.java
+++ Test/src/torai/test/Test1.java
@@ -4,5 +4,6 @@ public class Test1 {
 	public static void main(String[]args) {
 		System.out.println("Hello world");
 			// delete all comment except this row
+		System.out.println("Test Again");
 		}
 }
