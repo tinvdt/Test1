15b547f63a56b2cfd56a9258b252fdee90c99f78
tinvdt <thanhtin1808@gmail.com>
2015-03-16 13:47:12 +0700

    Update Test1.java

--- Test/src/torai/test/Test1.java
+++ Test/src/torai/test/Test1.java
@@ -3,9 +3,6 @@ package torai.test;
 public class Test1 {
 	public static void main(String[]args) {
 		System.out.println("Hello world");
-		// tin comment
-		// tin commit on server
-		// tin comment 2
-		// tin commit on server 2
+			// delete all comment except this row
 		}
 }
