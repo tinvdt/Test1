69220e2e05de0ecacefa0523b2f4ce38a2d5ee72
tinvdt <thanhtin1808@gmail.com>
2015-03-16 13:34:27 +0700

    commit

---
+++ Test/src/torai/test/Test1.java
@@ -0,0 +1,17 @@
+package torai.test;
+
+public class Test1 {
+	public static void main(String[]args) {
+//		String sql = "SELECT SERV.APP_SERVICE_ID, SERV.SERVICE_DESC, SERV.SERVICE_APPLICATION_NAME, SERV.SERVICE_LOCATION, " +
+//				"SERV.APP_SERVICE_TYPE_ID, SERV.SERVICE_NAME, SERV.GROUP_ID, ASG.NAME, " +
+//				"LOC.APP_SERVICE_LOCATION_ID, LOC.APP_SERVER_PORT_ID, LOC.APP_SERVER_SECURITY_ID, " +
+//				"LOC.APP_JVM_ID, LOC.DISABLED " +
+//				"FROM APP_SERVICES SERV, APP_SERVICE_LOCATIONS LOC, APP_SERVICE_GROUPS ASG " +
+//				"WHERE SERV.APP_SERVICE_ID = LOC.APP_SERVICE_ID(+) AND " +
+//				"SERV.GROUP_ID = ASG.APP_SERVICE_GROUP_ID(+) ";
+//		System.out.println(sql);
+		
+		System.out.println("Hello world");
+		// tin comment
+		}
+}
